191ec21cd8aa000067aff65d3c9b767ad0b27418
Eduardo Cerqueira <eduardocerqueirasilva@gmail.com>
2022-06-20 21:24:35 -0300

    Criado os métodos

--- src/main/java/com/example/project/Turma.java
+++ src/main/java/com/example/project/Turma.java
@@ -1,21 +1,45 @@
 package com.example.project;
 
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 public class Turma {
-    
+
+    LinkedList<Double> lista = new LinkedList<>();
+
     public void addNota(double nota) {
-        //teste
-        return;
+        lista.add(nota);
+        //return ;
     }
 
-    public int getQuantidadeNotas() {
-        return 0;
+    public double getQuantidadeNotas() {
+        double tst = lista.size();
+        return tst;
+
     }
 
     public double getMediaDasNotas() {
-        return 0.0;
+        Double notas = 0.0;
+        ListIterator<Double> listIterator = lista.listIterator();
+
+        while (listIterator.hasNext()) {
+            notas += listIterator.next();
+            
+        }
+        return notas / getQuantidadeNotas();
     }
 
     public int getQuantidadeNotasMeioresQueAMedia() {
-        return 0;
+        int i = 0;
+        ListIterator<Double> listIterator = lista.listIterator();
+
+        while (listIterator.hasNext()) {
+            if(listIterator.next()>getMediaDasNotas()){
+                i++;
+            }
+            
+        }
+        
+        return i;
     }
 }
